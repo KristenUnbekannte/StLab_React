57fa6c7ddd2fc6ca6c1cbb1d2cba42fdbde85f6e
Kristina_Platkovskaya <kristina.platkovskaya@mail.ru>
2018-08-03 19:43:02 +0300

    Deleted array from ParentContainer

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
 import ParentContainer from './containers/ParentContainer';
-import Menu from './containers/MenuContainer';
-import About from './views/About';
-import LoginContainer from './containers/LoginContainer';
-import LoginReduxContainer from './containers/LoginReduxContainer';
-import loginReducer from './reducers/LoginReduxReducer';
-import LoginSuccess from './views/Login-success';
-import LoginReduxFormContainer from './containers/LoginReduxFormContainer';
-
-
-const reducers = combineReducers({ login: loginReducer, form: formReducer });
-const store = createStore(reducers);
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <HashRouter>
-          <Switch>
-            <Route path="/not_found" children={() => <h2>404 - страница не найдена</h2>} />
-            <Route>
-              <div>
-                <Menu />
-                <Switch>
-                  <Route exact path="/" />
-                  <Route exact path="/about" component={About} />
-                  <Route exact path="/counters" component={ParentContainer} />
-                  <Route exact path="/login" component={LoginContainer} />
-                  <Route exact path="/login-redux" component={LoginReduxContainer} />
-                  <Route exact path="/login-redux/success" component={LoginSuccess} />
-                  <Route exact path="/login-redux-form" component={LoginReduxFormContainer} />
-                  <Route exact path="/login-redux-form/success" component={LoginSuccess} />
-                  <Redirect to="/not_found" />
-                </Switch>
-              </div>
-            </Route>
-          </Switch>
-        </HashRouter>
-      </Provider>
-    );
-  }
+	render() {
+		return (
+			<ParentContainer />
+		);
+	}
 }
 
 export default App;
--- src/views/Counter/style.js
+++ src/views/Counter/style.js
@@ -1,15 +1,14 @@
 export default theme => ({
-  button: {
-    margin: theme.spacing.unit,
-  },
-  root: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-  },
-  card: {
-    maxWidth: 340,
-    margin: '20px auto 0 auto',
-  },
+	button: {
+		margin: theme.spacing.unit,
+	},
+	root: {
+		...theme.mixins.gutters(),
+		paddingTop: theme.spacing.unit * 2,
+		paddingBottom: theme.spacing.unit * 2,
+	},
+	card: {
+		maxWidth: 340,
+		margin: '20px auto 0 auto',
+	},
 });
-
